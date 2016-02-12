6cf3533cfb26eae9b3b59795b644a93a93e2d19a
auxo <auxo86@gmail.com>
2016-02-12 21:42:32 +0800

    解決pattern要隨時對調的問題 但是衍伸了新的問題@@

--- SourceReader.py
+++ SourceReader.py
@@ -35,34 +35,35 @@ def ReturnMemberChange(conn):
     return listMemberChange
 
 def RecordNamesAndPattern(listMemberForArrange, intStartMemberId):
-    listPattern=[]
-    #先看看排到的這個人在listMemberForArrange中出現幾次
-    listIdsAndNames = list(filter(lambda Names: Names[1] == listMemberForArrange[intStartMemberId - 1][1], listMemberForArrange))
-    #如果抓出來的list中有兩個元素，從第一個元素開始，如果ID = 現在排到的人的ID，則把listPattern的list塞入一個1。當然另外一個就是塞入0，如果抓出的元素只有一個，那麼listPattern只會有一個1。這可以告訴我們，如果是明旭，是前面的明旭還是後面的明旭
-    for IDandName in listIdsAndNames:
-        if IDandName[0] == intStartMemberId:
-            listPattern.append(1)
-        else:
-            listPattern.append(0)
-    listNowAndNextMemberNamesAndPattern = [[],]
+    listNowAndNextMemberNamesAndPattern = []
+    #迴圈跑十次，保留十個人名
     for i in range(0,10,1):
-        listNowAndNextMemberNamesAndPattern[0].append(listMemberForArrange[intStartMemberId - 1][1])
+		#先看看排到的這個人在listMemberForArrange中出現幾次
+        listIdsAndNames = list(filter(lambda Names: Names[1] == listMemberForArrange[intStartMemberId - 1][1], listMemberForArrange))
+		#如果抓出來的list中有兩個元素，從第一個元素開始，如果ID = 現在排到的人的ID，則把listPattern的list塞入一個1。當然另外一個就是塞入0，如果抓出的元素只有一個，那麼listPattern只會有一個1。這可以告訴我們，如果是明旭，是前面的明旭還是後面的明旭，也就是製造pattern
+        listPattern=[]
+        for IDandName in listIdsAndNames:
+            if IDandName[0] == intStartMemberId:
+                listPattern.append(1)
+            else:
+                listPattern.append(0)
+        listNowAndNextMemberNamesAndPattern.append((listMemberForArrange[intStartMemberId - 1][1],) + (listPattern,))
         intStartMemberId += 1
         if intStartMemberId > len(listMemberForArrange):
             intStartMemberId = 1
-    listNowAndNextMemberNamesAndPattern.append(listPattern)
-    #listNowAndNextMemberNamesAndPattern裡面大概是這樣 [['明旭','慧枚',....], [1,0]]
+    #listNowAndNextMemberNamesAndPattern裡面大概是這樣 [('明旭', [1,0]), ('慧枚', [1,0])....]
     return listNowAndNextMemberNamesAndPattern
 
 def GetBackStarterID(listNowAndNextMemberNamesAndPattern, listMemberForArrange):
     listIdsAndNames = []
     i = 0
-    #保留十個人名，萬一一個個被抽走，還可以有十個機會。如果超過十個就會出現錯誤
+    listZipped = []
+    #有保留十個人名，萬一一個個被抽走，還可以有十個機會。如果超過十個就會出現錯誤
     while len(listIdsAndNames) == 0:
-        listIdsAndNames = list(filter(lambda Names: Names[1] == listNowAndNextMemberNamesAndPattern[0][i], listMemberForArrange))
+        listIdsAndNames = list(filter(lambda Names: Names[1] == listNowAndNextMemberNamesAndPattern[i][0], listMemberForArrange))
+		#todo: zip後應該是像這樣[1, (145, '詠馨')]，但是有個問題，就是萬一這個人本來沒有折扣，如果pattern是[0,1]那就ok。但是萬一他在listMemberForArrange加加減減的過程中變成有折扣，這時候如果pattern還是[0,1]，這就糟了。zip後可能會產生[0, (145, '詠馨')]....於是在下面的程式碼中，就變成最後回傳的intStartMemberId == 0....@@
+        listZipped = zip(listNowAndNextMemberNamesAndPattern[i][1], listIdsAndNames)
         i += 1
-    #zip後應該是像這樣(1, (145, '詠馨'))
-    listZipped = zip(listNowAndNextMemberNamesAndPattern[1], listIdsAndNames)
     intStartMemberId = 0
     for item in listZipped:
         if item[0] == 1:
