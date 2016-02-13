28325ad8534860a68efc3779a678373f84d82753
auxo <auxo86@gmail.com>
2016-02-13 14:13:02 +0800

    #為了避免這個問題 -> 就是萬一這個人本來沒有折扣，如果pattern是[0,1]那就ok。但是萬一他在listMemberForArrange加加減減的過程中變成有折扣，這時候如果pattern還是[0,1]，這就糟了。zip後可能會產生[0, (145, '詠馨')]....於是在下面的程式碼中，就變成最後回傳的intStartMemberId == 0

--- SourceReader.py
+++ SourceReader.py
@@ -54,15 +54,22 @@ def RecordNamesAndPattern(listMemberForArrange, intStartMemberId):
     #listNowAndNextMemberNamesAndPattern裡面大概是這樣 [('明旭', [1,0]), ('慧枚', [1,0])....]
     return listNowAndNextMemberNamesAndPattern
 
+def CheckPattern(listZipped):
+    #為了避免這個問題 -> 就是萬一這個人本來沒有折扣，如果pattern是[0,1]那就ok。但是萬一他在listMemberForArrange加加減減的過程中變成有折扣，這時候如果pattern還是[0,1]，這就糟了。zip後可能會產生[0, (145, '詠馨')]....於是在下面的程式碼中，就變成最後回傳的intStartMemberId == 0
+    if (len(listZipped) == 1 and listZipped[0][0] == 0) or len(listZipped) == 0:
+        return 0
+    else:
+        return 1
+
 def GetBackStarterID(listNowAndNextMemberNamesAndPattern, listMemberForArrange):
     listIdsAndNames = []
     i = 0
     listZipped = []
     #有保留十個人名，萬一一個個被抽走，還可以有十個機會。如果超過十個就會出現錯誤
-    while len(listIdsAndNames) == 0:
+    #為了避免這個問題 -> 就是萬一這個人本來沒有折扣，如果pattern是[0,1]那就ok。但是萬一他在listMemberForArrange加加減減的過程中變成有折扣，這時候如果pattern還是[0,1]，這就糟了。zip後可能會產生[0, (145, '詠馨')]....於是在下面的程式碼中，就變成最後回傳的intStartMemberId == 0
+    while len(listIdsAndNames) == 0 or CheckPattern(listZipped) == 0:
         listIdsAndNames = list(filter(lambda Names: Names[1] == listNowAndNextMemberNamesAndPattern[i][0], listMemberForArrange))
-		#todo: zip後應該是像這樣[1, (145, '詠馨')]，但是有個問題，就是萬一這個人本來沒有折扣，如果pattern是[0,1]那就ok。但是萬一他在listMemberForArrange加加減減的過程中變成有折扣，這時候如果pattern還是[0,1]，這就糟了。zip後可能會產生[0, (145, '詠馨')]....於是在下面的程式碼中，就變成最後回傳的intStartMemberId == 0....@@
-        listZipped = zip(listNowAndNextMemberNamesAndPattern[i][1], listIdsAndNames)
+        listZipped = list(zip(listNowAndNextMemberNamesAndPattern[i][1], listIdsAndNames))
         i += 1
     intStartMemberId = 0
     for item in listZipped:
